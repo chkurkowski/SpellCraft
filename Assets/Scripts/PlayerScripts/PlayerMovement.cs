35c6a6aa393dd51d0bddc894b5708aa9d58ebdf2
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2018-10-05 03:39:45 -0500

    A good start
    Basic movement is implemented. Gave the character a circle collider to be able to slide off of frustrating corners easier. You'll also see in the code a *100 was added to both the x and y velocity, that was just so that we could have a smaller number for movementSpeed in the editor.

---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D playerRigidbody;
+    private float horizontalMovement;
+    private float verticalMovement;
+    public float movementSpeed = 5;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+	}
+
+    public void Movement()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal");
+        verticalMovement = Input.GetAxis("Vertical");
+        playerRigidbody.velocity = new Vector3(horizontalMovement * movementSpeed * Time.deltaTime * 100, verticalMovement * movementSpeed * Time.deltaTime * 100);
+    }
+}
