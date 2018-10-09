0aa270b135bb01e820e68b9a95df874d6d599c8f
Ty Loveland <tloveland98@gmail.com>
2018-10-09 14:51:17 -0500

    Dynamic Camera is working
    camera zooms out the farther you are from the boss. Can only zoom in so far in order to make sure it doesn't get too close

---
+++ Assets/Scripts/CameraScripts/DynamicCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicCamera : MonoBehaviour 
+{
+    public GameObject Player;
+    public GameObject Boss;
+    private float distance;
+    private float zoomSize;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        distance = Vector2.Distance(Player.transform.position, Boss.transform.position);
+        zoomSize = distance;
+        if(distance <=25)
+        {
+            GetComponent<Camera>().orthographicSize = 25;
+        }
+        else
+        {
+            GetComponent<Camera>().orthographicSize = zoomSize;
+        }
+
+    }
+}
