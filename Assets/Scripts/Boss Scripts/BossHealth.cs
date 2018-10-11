6986af4e8634babbea63be605e0cb5702857b47b
Ty Loveland <tloveland98@gmail.com>
2018-10-11 13:31:45 -0500

    getting there

---
+++ Assets/Scripts/Boss Scripts/BossHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BossHealth : MonoBehaviour
+{
+    //boss health bar
+    public Image bossHealthBar;
+    public GameObject boss;
+
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	}
+
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "Fireball(Clone)") 
+        {
+            bossHealthBar.fillAmount -= 10;
+        }
+    }
+}
