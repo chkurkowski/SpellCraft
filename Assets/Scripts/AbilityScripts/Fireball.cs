5a2b0d4c8ceb86936b2b76df38cb7716cdddaab1
Chase Kurkowski <zeke1498@gmail.com>
2018-10-17 15:03:23 -0500

    Removed any leftover errors

--- Assets/Scripts/AbilityScripts/Fireball.cs
+++ Assets/Scripts/AbilityScripts/Fireball.cs
@@ -3,20 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Fireball : MonoBehaviour {
-
-<<<<<<< HEAD
-
-    
 	
     public int damage = 5;
  
-	
-    // Use this for initialization
-    public void OnTriggerEnter2D(Collider2D col)
-=======
 	// Use this for initialization
     private void OnTriggerEnter2D(Collider2D col)
->>>>>>> parent of a7640f5... Merge pull request #5 from zeke1498/health-bar-branch
     {
         if(col.gameObject.tag == "Boss" && gameObject.tag == "Projectile")
         {
