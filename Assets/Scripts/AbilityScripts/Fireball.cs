ca396e382ddf80a39eb3aec2c8a3dabbce8391d7
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2018-10-17 10:41:38 -0500

    no idea what's going on

--- Assets/Scripts/AbilityScripts/Fireball.cs
+++ Assets/Scripts/AbilityScripts/Fireball.cs
@@ -6,17 +6,14 @@ using UnityEngine.UI;
 public class Fireball : MonoBehaviour 
 {
 
-<<<<<<< HEAD
+
     
-	// Use this for initialization
-    private void OnTriggerEnter2D(Collider2D col)
-=======
+	
     public int damage = 5;
  
 	
     // Use this for initialization
     public void OnTriggerEnter2D(Collider2D col)
->>>>>>> a7640f54bb3a55c0e7df9aad063c0c8736cdd9d7
     {
         if(col.gameObject.tag == "Boss")
         {
