931905bfcfa11d63b107b0cc4e55709d1736ca12
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2018-10-22 07:07:38 -0500

    hopefullyNotBrokenCode
    swapped Coroutine for Update

--- Assets/Scripts/Boss Scripts/BossPositionLimiter.cs
+++ Assets/Scripts/Boss Scripts/BossPositionLimiter.cs
@@ -12,38 +12,37 @@ public class BossPositionLimiter : MonoBehaviour {
 	void Start ()
     {
         bossHealthInfo = gameObject.GetComponent<BossHealth>();
-        StartCoroutine("PositionChecker");
+       
 	}
-	
-	IEnumerator PositionChecker()
+
+    void Update()
     {
-        Debug.Log("Position CHecker started!");
-        while(bossHealthInfo.isAlive)
+        if(bossHealthInfo.isAlive)
         {
-            if(gameObject.transform.position.x > rightLimit.transform.position.x)
+            if (gameObject.transform.position.x > rightLimit.transform.position.x)
             {
                 Debug.Log("Right Limit Triggered!");
                 gameObject.transform.position = new Vector2(rightLimit.transform.position.x, gameObject.transform.position.y);
             }
 
-            if(gameObject.transform.position.x < leftLimit.transform.position.x)
+            if (gameObject.transform.position.x < leftLimit.transform.position.x)
             {
                 Debug.Log("Left Limit Triggered!");
                 gameObject.transform.position = new Vector2(leftLimit.transform.position.x, gameObject.transform.position.y);
             }
 
-            if(gameObject.transform.position.y < bottomLimit.transform.position.y)
+            if (gameObject.transform.position.y < bottomLimit.transform.position.y)
             {
                 Debug.Log("Bottom Limit Triggered!");
                 gameObject.transform.position = new Vector2(gameObject.transform.position.y, bottomLimit.transform.position.y);
             }
 
-            if(gameObject.transform.position.y > topLimit.transform.position.y)
+            if (gameObject.transform.position.y > topLimit.transform.position.y)
             {
                 Debug.Log("Top Limit Triggered!");
                 gameObject.transform.position = new Vector2(gameObject.transform.position.y, topLimit.transform.position.y);
             }
         }
-        yield return null;
     }
+
 }
