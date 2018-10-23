25c6492f371e79f118cb77afb51bc3014b5338d0
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2018-10-23 01:25:37 -0500

    noice

---
+++ Assets/Scripts/UiPopUps/tutorialPopUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialPopUp : MonoBehaviour {
+    public GameObject tutorialGhost;
+	// Use this for initialization
+	void Start ()
+    {
+        tutorialGhost.SetActive(false);	
+	}
+
+    // Update is called once per frame
+
+    private void OnTriggerEnter2D(Collider2D trig)
+    {
+        if(trig.gameObject.tag == "Player")
+        {
+            tutorialGhost.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D trig)
+    {
+        if (trig.gameObject.tag == "Player")
+        {
+            tutorialGhost.SetActive(false);
+        }
+    }
+}
+
