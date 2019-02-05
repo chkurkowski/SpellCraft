8b18c675c6f3ef70f9fd2ce6cd7fc2736b609c08
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-02-04 21:09:16 -0600

    safe merge.jpg?

--- Assets/Scripts/CameraScripts/ArenaCameraActivator.cs
+++ Assets/Scripts/CameraScripts/ArenaCameraActivator.cs
@@ -2,13 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArenaCameraActivator : MonoBehaviour {
+public class ArenaCameraActivator : MonoBehaviour
+{
+    public GameObject mainCamera;
+    /// <summary>
+    /// type the name of the boss that the arena belongs to
+    /// </summary>
+
+    public string associatedBossName = "";
+
+    public GameObject prototypeBoss;
+    public GameObject pylonBoss;
+    public GameObject alchemistBoss;
+    public GameObject lichBoss;
+    public GameObject reflectorBoss;
+    public GameObject charmerBoss;
+
+
+    private void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera");
+
+        switch (BossName)
+        {
+            case "Lich":
+                
+                break;
+
+            case "Pylon":
+               
+                break;
+
+            case "Charmer":
+               
+                break;
+
+            case "Reflector":
+               
+                break;
+
+            case "Alchemist":
+             
+                break;
+
+            case "PrototypeBoss":
+              
+                break;
+
+        }
+    }
+
+
 
     private void OnTriggerEnter2D(Collider2D trig)
     {
         if(trig.gameObject.tag == "Player")
         {
             GameObject.Find("Main Camera").GetComponent<CameraScriptActivator>().ActivateDynamicCamera();
+            //mainCamera.GetComponent<DynamicCamera>().Boss = 
         }
     }
 
