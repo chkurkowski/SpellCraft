b34414a5efe7bcdef0270e1c281b03262e1f8f6a
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-02-10 00:50:47 -0600

    pylonBoss Particles and Reflect

--- Assets/Scripts/Boss Scripts/PylonScripts/PylonMovement.cs
+++ Assets/Scripts/Boss Scripts/PylonScripts/PylonMovement.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PylonMovement : MonoBehaviour {
-//TODO 
-//write a function for spinning that takes a time variable for the laser.
-//also write a function that can be called to increase the rate at which the boss spins.
-	// Use this for initialization
-	void Start () {
-		
+    //TODO 
+    //write a function for spinning that takes a time variable for the laser.
+    //also write a function that can be called to increase the rate at which the boss spins.
+    // Use this for initialization
+
+    public float rotationIncrement;
+    public float rotationAmount;
+
+    void Start ()
+    {
+        InvokeRepeating("PylonRotate", 0, rotationIncrement);
 	}
 	
 	// Update is called once per frame
@@ -16,4 +21,10 @@ public class PylonMovement : MonoBehaviour {
     {
 		
 	}
+
+    public void PylonRotate()
+    {
+        gameObject.transform.Rotate(0, 0, rotationAmount);
+    }
+
 }
