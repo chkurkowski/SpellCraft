1019ebb4cd8cc1d5ad3fa1d5703b0011ebb10880
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-02-11 15:08:36 -0600

    tons of pylonCode AND objectPooler

--- Assets/Scripts/ProjectileScripts/MegaBomb.cs
+++ Assets/Scripts/ProjectileScripts/MegaBomb.cs
@@ -40,9 +40,13 @@ public class MegaBomb : MonoBehaviour {
         }
         else if (col.gameObject.tag == "Absorb")
         {
-            GameObject.Find("Player").GetComponent<PlayerHealth>().DamagePlayer(bombDamage / 2);
+            GameObject.Find("Player").GetComponent<PlayerHealth>().HealPlayer(bombDamage / 2);
             GameObject.Find("Player").GetComponent<PlayerHealth>().playerHealthBar.fillAmount += .025f;
             Destroy(gameObject);
+        }
+        else if (col.gameObject.tag == "CameraTrigger")
+        {
+            //do nothing
         }
         else if (col.gameObject.tag != "Boss" || gameObject.tag != "CameraTrigger" || col.gameObject.tag == "Player")
         {
