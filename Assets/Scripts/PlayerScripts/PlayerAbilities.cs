5fa98cf5ff6b929411eb043fd3f1c0ec8a59d10c
Chase Kurkowski <chk@fastmail.com>
2019-02-13 00:20:45 -0600

    HealStun further developed

--- Assets/Scripts/PlayerScripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerScripts/PlayerAbilities.cs
@@ -25,10 +25,6 @@ public class PlayerAbilities : MonoBehaviour {
 	}
 	public State state;
 
-	//Booleans
-	private bool isBurst;
-	private bool evadeCalled;
-
 	private List<string> lastAttacks = new List<string>();
 	private List<string> ritualList = new List<string>();
 
@@ -49,8 +45,6 @@ public class PlayerAbilities : MonoBehaviour {
 
 	private void Start()
 	{
-		isBurst = false;
-        evadeCalled = false;
         burstTimer = BURSTCOOLDOWN;
         evadeTimer = EVADECOOLDOWN;
 
