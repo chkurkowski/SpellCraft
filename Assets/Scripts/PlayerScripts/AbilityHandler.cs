a15e0c62bd54aeb9590f52d1a85ee687be71e3e4
Chase Kurkowski <chk@fastmail.com>
2019-02-14 01:54:31 -0600

    Sound Movement

--- Assets/Scripts/PlayerScripts/AbilityHandler.cs
+++ Assets/Scripts/PlayerScripts/AbilityHandler.cs
@@ -117,11 +117,12 @@ public class AbilityHandler : MonoBehaviour {
 
     private void Reflect()
     {
-         reflectAudio = GetComponent<AudioSource>();
-         reflectAudio.clip = reflectLoopSound;
-         reflectAudio.Play();
         if (reflectTimer >= REFLECTCOOLDOWN)
         {
+            reflectAudio = GetComponent<AudioSource>();
+            reflectAudio.clip = reflectLoopSound;
+            reflectAudio.Play();
+            
             reflect.SetActive(true);
             reflectTimer = 0;
             reflectAudio.Stop();
@@ -142,11 +143,11 @@ public class AbilityHandler : MonoBehaviour {
     //NewName - AtkSim
     private void AttackSim(bool isBurst)
     {
-         abilityHandlerSource.clip = attackSimSound;
-         abilityHandlerSource.PlayOneShot(attackSimSound);
         if (isBurst)
         {
             //TODO Add AttackSim Burst Sound
+            abilityHandlerSource.clip = attackSimSound;
+            abilityHandlerSource.PlayOneShot(attackSimSound);
 
             GameObject sim = Instantiate(simulacrum, transform.position, Quaternion.identity);
             sim.GetComponent<SimulacrumAbilities>().type = "Attack";
