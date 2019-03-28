fc33f34aa20a5a071bc5736f3a869e48b7eec25c
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-03-28 03:44:10 -0500

    actuarial science

---
+++ Assets/Scripts/EnvironmentScripts/TutorialManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public int tutorialStage = 0;
+    public GameObject dummyOne;
+    public GameObject dummyTwo;
+    public GameObject dummyThree;
+    public GameObject dummyFour;
+
+    public GameObject tutorialRing;
+    public GameObject tutorialDoor;
+
+    private void Start()
+    {
+        dummyOne.SetActive(false);
+        dummyTwo.SetActive(false);
+        dummyThree.SetActive(false);
+        dummyFour.SetActive(false);
+    }
+
+    private void Update()
+    {
+        switch (tutorialStage)
+        {
+            case 1:
+                dummyOne.SetActive(true);
+                break;
+        }
+    }
+
+    public void NextTutorialStage()
+    {
+        tutorialStage++;
+    }
+
+}
