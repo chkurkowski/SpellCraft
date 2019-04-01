08bd773d9004ff0bab66492793707600b688d114
Ty Loveland <tloveland98@gmail.com>
2019-04-01 17:24:54 -0500

    work in progress

---
+++ Assets/overworldMusicHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class overworldMusicHandler : MonoBehaviour 
+{
+
+    public bool isPlayingMusic = false;
+    public AudioSource overworldMusic;
+    public GameObject pylonHB;
+
+	// Use this for initialization
+	void Start () 
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (pylonHB.activeInHierarchy == false)
+        {
+            isPlayingMusic = true;
+            overworldMusic.Play();
+        }
+
+        else if (pylonHB.activeInHierarchy == true)
+        {
+            overworldMusic.Stop();
+            isPlayingMusic = false;
+        }
+    }
+}
