03902fb0922cafa28bf3ccad5c97941c9f82f07c
Ty Loveland <tloveland98@gmail.com>
2019-04-01 20:53:33 -0500

    overworld music complete

--- Assets/overworldMusicHandler.cs
+++ Assets/overworldMusicHandler.cs
@@ -1,33 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class overworldMusicHandler : MonoBehaviour 
+public class OverworldMusicHandler : MonoBehaviour
 {
-
-    public bool isPlayingMusic = false;
+    public bool isPlayingOwMusic = false;
     public AudioSource overworldMusic;
-    public GameObject pylonHB;
-
-	// Use this for initialization
-	void Start () 
+    private BossHealth health;
+    public Image pylonHealthBar;
+    public Image lichHealthBar;
+    // Use this for initialization
+    void Start()
     {
 
     }
-	
-	// Update is called once per frame
-	void Update () 
-    {
-        if (pylonHB.activeInHierarchy == false)
-        {
-            isPlayingMusic = true;
-            overworldMusic.Play();
-        }
 
-        else if (pylonHB.activeInHierarchy == true)
+    // Update is called once per frame
+    void Update()
+    {
+        if (pylonHealthBar.isActiveAndEnabled)
+        {
+            isPlayingOwMusic = false;
+            overworldMusic.Stop();
+        }
+        else if (!pylonHealthBar.isActiveAndEnabled)
+        {
+            if(!isPlayingOwMusic)
+            {
+                isPlayingOwMusic = true;
+                overworldMusic.Play();
+            }
+        }
+        else if (pylonHealthBar.isActiveAndEnabled && pylonHealthBar.fillAmount > 0 )
         {
             overworldMusic.Stop();
-            isPlayingMusic = false;
+            isPlayingOwMusic = true;     
+        }
+
+
+
+
+        if (lichHealthBar.isActiveAndEnabled)
+        {
+            isPlayingOwMusic = false;
+            overworldMusic.Stop();
+        }
+        else if (!lichHealthBar.isActiveAndEnabled)
+        {
+            if (!isPlayingOwMusic)
+            {
+                isPlayingOwMusic = true;
+                overworldMusic.Play();
+            }
+        }
+        else if (lichHealthBar.isActiveAndEnabled && pylonHealthBar.fillAmount > 0)
+        {
+            overworldMusic.Stop();
+            isPlayingOwMusic = true;
         }
     }
+   
+
+    //public void PlayOWMusic(Image pylonHealthBar)
+   // {
+
+  //  }
+
+
 }
+
+        
+
+
