e33d9619a80bf6dfa3a882590d919bdd78c5e480
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-04-14 00:24:46 -0500

    fixed TestinMaster

--- Assets/Scripts/ProjectileScripts/MagicBall.cs
+++ Assets/Scripts/ProjectileScripts/MagicBall.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //This is the Magic Missile Projectile
-public class MagicBall : MonoBehaviour {
+public class MagicBall : MonoBehaviour
+{
 
     private ProjectileDamage projectileDamageInfo;
     private float stunDamage;
-   
+
     public bool firedFromPlayer = true;
     public bool isAttackSimMissile = false;
     public bool isSplitSimMissile = false;
@@ -28,36 +29,46 @@ public class MagicBall : MonoBehaviour {
 
     void Update()
     {
-        if(!firedFromPlayer)
+        if (!firedFromPlayer)
         {
             transform.Translate(Vector2.up * Time.deltaTime * magicBallSpeed);
         }
-        else if(reflected && canReflect)
+        else if (reflected && canReflect)
         {
             canReflect = false;
             gameObject.GetComponent<Rigidbody2D>().velocity = gameObject.GetComponent<Rigidbody2D>().velocity * -1;
         }
-      
+
     }
 
     // Use this for initialization
     private void OnTriggerEnter2D(Collider2D col)
     {
-      //  Debug.Log("PLAYER PROJECTILE HIT: " + col.transform.name + "with tag: " + col.transform.tag);
-        if(col.GetComponent<Collider2D>().transform.tag == null)
+        //  Debug.Log("PLAYER PROJECTILE HIT: " + col.transform.name + "with tag: " + col.transform.tag);
+        if (col.GetComponent<Collider2D>().transform.tag == null)
         {
-           Debug.Log("PLAYER PROJECTILE HIT UNTAGGED OBJECT ");
-            Instantiate(vfxObject, transform.position, transform.rotation);
+            Debug.Log("PLAYER PROJECTILE HIT UNTAGGED OBJECT ");
+            GameObject vfx = Instantiate(vfxObject, transform.position, transform.rotation);
+            if (magicBallDamage > .5)
+            {
+                vfx.GetComponent<SpriteRenderer>().color = Color.yellow;
+            }
+
             Destroy(gameObject);
         }
-        else if(col.gameObject.name == "Bomb")
+        else if (col.gameObject.name == "Bomb")
         {
-            Instantiate(vfxObject, transform.position, transform.rotation);
+            GameObject vfx = Instantiate(vfxObject, transform.position, transform.rotation);
+            if (magicBallDamage > .5)
+            {
+                vfx.GetComponent<SpriteRenderer>().color = Color.yellow;
+            }
+
             Destroy(gameObject);
         }
         else if (col.gameObject.tag == "EnemyReflect")
         {
-           // Debug.Log("enemy reflect should occur");
+            // Debug.Log("enemy reflect should occur");
             reflected = true;
             gameObject.tag = "EnemyProjectile";
             gameObject.layer = 9; //changes physics layers, do not touch or I stab you
@@ -66,25 +77,33 @@ public class MagicBall : MonoBehaviour {
         if (col.gameObject.tag == "Boss")
         {
             // Debug.Log(gameObject.name + " was destroyed by Boss:" + col.gameObject.name);
-            Instantiate(vfxObject, transform.position, transform.rotation);
+            GameObject vfx = Instantiate(vfxObject, transform.position, transform.rotation);
+            if (magicBallDamage > .5)
+            {
+                vfx.GetComponent<SpriteRenderer>().color = Color.yellow;
+            }
             Destroy(gameObject);
         }
-        else if (col.gameObject.tag == "Vortex" || col.gameObject.tag == "EnemyProjectile" 
+        else if (col.gameObject.tag == "Vortex" || col.gameObject.tag == "EnemyProjectile"
             || col.gameObject.tag == "Projectile" || col.gameObject.tag == "Split" || col.gameObject.tag == "CheckPoint" || col.gameObject.tag == "LaserEndPoint")
         {
-           
+
             //do nothing
         }
         else if (col.gameObject.tag != "Player" && col.gameObject.tag != "Reflect" && col.gameObject.tag != "Simulacrum" && col.gameObject.tag != "EnemyReflect" && col.gameObject.tag != "CameraTrigger")
         {
-            if (col.gameObject.tag != "Boss" || col.gameObject.tag != "CameraTrigger" || col.gameObject.tag != "HealStun" )
+            if (col.gameObject.tag != "Boss" || col.gameObject.tag != "CameraTrigger" || col.gameObject.tag != "HealStun")
             {
                 // Debug.Log(gameObject.name + " was destroyed by " + col.gameObject.name + "with tag :" + col.gameObject.tag );
-                Instantiate(vfxObject, transform.position, transform.rotation);
+                GameObject vfx = Instantiate(vfxObject, transform.position, transform.rotation);
+                if (magicBallDamage > .5)
+                {
+                    vfx.GetComponent<SpriteRenderer>().color = Color.yellow;
+                }
                 Destroy(gameObject);
             }
 
         }
-       
+
     }
 }
