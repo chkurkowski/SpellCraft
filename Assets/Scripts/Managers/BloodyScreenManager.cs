18db1b54a65c4cdd91bbdf3e656b03d46fa1970e
EdouardBorissov <36777343+EdouardBorissov@users.noreply.github.com>
2019-04-15 17:42:18 -0500

    bloodScreenStart

---
+++ Assets/Scripts/Managers/BloodyScreenManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloodyScreenManager : MonoBehaviour
+{
+    public GameObject bloodyScreenObject;
+    private SpriteRenderer bloodyScreen;
+    private PlayerHealth playerHealthInfo;
+    private bool isBleeding = false;
+    public float bleedSpeed = .1f;
+	// Use this for initialization
+	void Start ()
+    {
+        playerHealthInfo = GameObject.Find("Player").GetComponent<PlayerHealth>();
+        bloodyScreen = bloodyScreenObject.GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(!isBleeding && (playerHealthInfo.playerHealth / playerHealthInfo.maxPlayerHealth) <= 33f)
+        {
+            isBleeding = true;
+            InvokeRepeating("BleedScreen", 0, bleedSpeed);
+        }
+	}
+
+
+    void BleedScreen()
+    {
+        if ((playerHealthInfo.playerHealth / playerHealthInfo.maxPlayerHealth) <= 33f)
+        {
+            
+        }
+        else
+        {
+            isBleeding = false;
+            CancelInvoke("BleedScreen");
+        }
+    }
+}
