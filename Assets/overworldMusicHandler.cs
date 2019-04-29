038c7d64f52a9b5bafca56a7a1bc308b39843274
Ty Loveland <tloveland98@gmail.com>
2019-04-28 23:58:47 -0500

    updated a bunch of sfx
    pillars n attack 2 need implementation

--- Assets/overworldMusicHandler.cs
+++ Assets/overworldMusicHandler.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OverworldMusicHandler : MonoBehaviour
+public class overworldMusicHandler : MonoBehaviour
 {
     public bool isPlayingOwMusic = false;
     public AudioSource overworldMusic;
     private BossHealth health;
     public Image pylonHealthBar;
-//    public Image lichHealthBar;
+
     // Use this for initialization
     void Start()
     {
@@ -40,26 +40,8 @@ public class OverworldMusicHandler : MonoBehaviour
 
 
 
-
- //       if (lichHealthBar.isActiveAndEnabled)
- //      {
- //          isPlayingOwMusic = false;
- //          overworldMusic.Stop();
- //       }
- //       else if (!lichHealthBar.isActiveAndEnabled)
- //       {
- //          if (!isPlayingOwMusic)
- //           {
- //               isPlayingOwMusic = true;
- //               overworldMusic.Play();
- //           }
- //       }
- //       else if (lichHealthBar.isActiveAndEnabled && pylonHealthBar.fillAmount > 0)
- //       {
- //           overworldMusic.Stop();
- //           isPlayingOwMusic = true;
- //       }
     }
+
    
 
 
